e72ca815f9176ee0b2782c6c5fb3a15b9d58d13a
Pablo Nussembaum <baunax@gmail.com>
2011-03-29 17:08:40 -0300

    added drools-gorm plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.plugin.location.'drools-gorm' = "/home/bauna/dev/work.bauga.force5/drools-gorm-ng"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
